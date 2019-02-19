2bafb75d45ba38fe68ff8409c7d3adcebfb7a5ec
jessikalayne <jessika.layne.pereira@gmail.com>
2019-02-18 21:20:40 -0300

    Twitter 0.0.2 Ok

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+const MAX_LENGTH = 140;
+
 var twitterText = document.querySelector ('#twitterText');
 var tweetButton = document.querySelector ('#tweetButton');
 var twitterList = document.querySelector ('#twitterList');
+var twitterCounter = document.querySelector ('#twitterCounter');
 
 
 tweetButton.addEventListener('click', addTweet);
@@ -9,6 +12,17 @@ function addTweet(event) {
     var newLi = document.createElement('li');
     newLi.textContent = twitterText.value;
     twitterList.appendChild(newLi);
-    twitterText.value = '';
-    
+    twitterText.value = ''; 
+}
+
+twitterText.addEventListener('keyup', changeCounter);
+
+function changeCounter(event){
+    var counterValue = MAX_LENGTH - twitterText.value.length;
+    twitterCounter.textContent = counterValue;
+    if (counterValue === MAX_LENGTH) {
+        tweetButton.setAttribute('disabled', '');
+    }else {
+        tweetButton.removeAttribute ('disabled');
+    }
 }
