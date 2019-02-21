7083eaa6deb735fe84ca9ae6e21dcee8ea57e51a
jessikalayne <jessika.layne.pereira@gmail.com>
2019-02-21 14:38:00 -0300

    Twitter 0.0.8 - Atualização CSS e HTML 2

--- index.js
+++ index.js
@@ -5,23 +5,25 @@ let tweetButton = document.querySelector ('#tweetButton');
 let twitterList = document.querySelector ('#twitterList');
 let twitterCounter = document.querySelector ('#twitterCounter');
 
+
 tweetButton.addEventListener('click', addTweet);
 
 function addTweet(event) {
     let newLi = document.createElement('li');
+    newLi.classList.add("tweet");
     let dateString = new Date().toLocaleTimeString().substring(0,5);    
     let textEnters = twitterText.value.replace(/\n/g, '<br>');
     
-    newLi.innerHTML = '[' + dateString +'] ' + textEnters;
+    newLi.innerHTML = '(' + dateString +') ' + textEnters;
     twitterList.appendChild(newLi);
     twitterText.value = '';
-    changeCounter();    
+    changeCounter();
 }
 
 twitterText.addEventListener('keyup', changeCounter);
 
 function changeCounter(event){
-    var counterValue = MAX_LENGTH - twitterText.value.length;
+    let counterValue = MAX_LENGTH - twitterText.value.length;
     twitterCounter.textContent = counterValue;
     
     if (counterValue === MAX_LENGTH || counterValue < 0) {
@@ -30,16 +32,19 @@ function changeCounter(event){
         tweetButton.removeAttribute ('disabled');
     }    
     if (twitterText.value.length > 130) {
-        twitterCounter.setAttribute ('class', 'color3');
+        twitterCounter.setAttribute ('class', 'danger-color');
     }else if (twitterText.value.length > 120) {
-        twitterCounter.setAttribute ('class', 'color2');
+        twitterCounter.setAttribute ('class', 'warning-color');
     }else {
-        twitterCounter.setAttribute ('class', 'color1');        
+        twitterCounter.setAttribute ('class', 'initial-color');        
     }    
+    
+    let lines = twitterText.value.split('\n');
     let linesCount = 0;
+    
     for(let i = 0; i < lines.length; i++) {
         let line = lines[i];
-        linesCount += Math.max(Math.ceil(line.length / 100), 5);
+        linesCount += Math.max(Math.ceil(line.length / 50), 1);
     }    
     twitterText.setAttribute('rows', linesCount);
 }
